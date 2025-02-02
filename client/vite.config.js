707ab4e8400ac2754350c2f6e27cf0ce92e478a5
Pradheep S <sivampradheep@gmail.com>
2025-02-02 11:03:42 +0530

    Fix: Clean install dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",  // Ensures correct asset paths in production
   build: {
     outDir: 'dist',  // Ensure output is in 'dist/'
   },
